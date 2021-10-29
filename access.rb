f280367c713e9296e92684dd9880b13345468a58
naoto-ysd <61533915+naoto-ysd@users.noreply.github.com>
2021-10-29 23:58:00 +0900

    operation comfirmation

--- access.rb
+++ access.rb
@@ -1,6 +1,5 @@
 class User
-  attr_reader :name
-  attr_reader :age
+  attr_accessor :name, :age
 
   def initialize(name, age)
     @name = name
@@ -8,6 +7,25 @@ class User
   end
 end
 
+class Article
+  def initialize(title,content)
+    @title = title
+    @content = content
+  end
+
+  def title
+    @title
+  end
+
+  def content
+    @content
+  end
+end
+
 user = User.new("hoge", 10)
 p user.name
 p user.age
+
+article = Article.new("本日のニュース","株価上昇")
+p article.title
+p article.content
